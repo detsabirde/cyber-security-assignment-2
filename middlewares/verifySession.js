dfc825de49a9eeaf7ce8fba375cb99c6776d8ed9
karim brahimi <karimbrahimi102@gmail.com>
2025-04-12 00:57:59 +0100

    logging

--- middlewares/verifySession.js
+++ middlewares/verifySession.js
@@ -1,8 +1,8 @@
 const session = require("express-session");
 
 function verifySession(req, res, next) {
-  if (req.session.user) {
-    console.log("Session exists for user:", req.session.user);
+  if (req.session && req.session.user) {
+    console.log("Session exists for user:", req.session);
     next();
   } else {
     res.redirect("/login");
